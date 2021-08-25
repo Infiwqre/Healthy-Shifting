13214a336ef43195d0d3241c508eea937f89f66c
Infiware <86568456+InfiwareGit@users.noreply.github.com>
2021-08-25 12:06:46 -0400

    HealthyTwerking Update 1.0

--- main/java/me/infiware/healthytwerking/HealthyTwerking.java
+++ main/java/me/infiware/healthytwerking/HealthyTwerking.java
@@ -9,6 +9,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.TimeUnit;
+
 public final class HealthyTwerking extends JavaPlugin implements Listener {
 
     @Override
@@ -20,11 +22,13 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
 
         getCommand("twerkHealth").setExecutor(this);
         getCommand("noTwerk").setExecutor(this);
+        getCommand("twerkDelay").setExecutor(this);
 
         getServer().getPluginManager().registerEvents(this,this);
     }
 
     public double twerkHealth = 1.0;
+    public long twerkDelayTime = 0;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -62,6 +66,23 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
                 System.out.println("You must be a player to use this command");
             }
 
+        }else if(command.getName().equalsIgnoreCase("twerkDelay")){
+            if (sender instanceof Player) {
+                if (args.length > 0) {
+                    try {
+                        twerkDelayTime = Long.parseLong(args[0]);
+                        p.sendMessage(ChatColor.DARK_GREEN + "There is now a " + ChatColor.RED + twerkDelayTime + ChatColor.DARK_GREEN + " millisecond delay");
+                    } catch (IllegalArgumentException e) {
+                        p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
+                        p.sendMessage(ChatColor.RED + "Ex: 0, 69, 420 & etc.");
+                    }
+                } else {
+                    p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
+                    p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
+                }
+            }else{
+                System.out.println("You must be a player to use this command");
+            }
         }
 
 
@@ -73,16 +94,38 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onTwerkHeal(PlayerToggleSneakEvent event) {
+    public void onTwerkHeal(PlayerToggleSneakEvent event){
         Player p = event.getPlayer();
-        try {
-            p.setHealth(p.getHealth() + twerkHealth);
-        }
-        catch(Exception e){
-            double currentHealth = p.getHealth();
-            currentHealth = 20 - currentHealth;
+        if (twerkDelayTime == 0) {
+            try {
+                p.setHealth(p.getHealth() + twerkHealth);
+            }
+            catch(Exception e){
+                double currentHealth = p.getHealth();
+                currentHealth = 20 - currentHealth;
 
-            p.setHealth(p.getHealth() + currentHealth);
+                p.setHealth(p.getHealth() + currentHealth);
+            }
+        }else if (twerkDelayTime > 0) {
+            try {
+                Thread.sleep(twerkDelayTime);
+                try {
+                    p.setHealth(p.getHealth() + twerkHealth);
+                }
+                catch(Exception e){
+                    double currentHealth = p.getHealth();
+                    currentHealth = 20 - currentHealth;
+
+                    p.setHealth(p.getHealth() + currentHealth);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }else if (twerkDelayTime < 0){
+            p.sendMessage(ChatColor.RED + "You must give a positive number as an argument");
+        }else{
+            p.sendMessage(ChatColor.RED + "Please give a valid argument");
+            p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
         }
     }
 
