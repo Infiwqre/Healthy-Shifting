a4fcba5023550541e7f9a4bb6d131c4142bcf756
Infiware <86568456+InfiwareGit@users.noreply.github.com>
2021-08-31 17:09:13 -0400

    Update 2.0
    Added configurable permissions for using commands
    - Added default twerk delay time which can be configured in config.yml
    - Added default twerk heal amount which can be configured in config.yml

--- main/java/me/infiware/healthytwerking/HealthyTwerking.java
+++ main/java/me/infiware/healthytwerking/HealthyTwerking.java
@@ -9,8 +9,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.concurrent.TimeUnit;
-
 public final class HealthyTwerking extends JavaPlugin implements Listener {
 
     @Override
@@ -24,11 +22,15 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
         getCommand("noTwerk").setExecutor(this);
         getCommand("twerkDelay").setExecutor(this);
 
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
         getServer().getPluginManager().registerEvents(this,this);
     }
 
-    public double twerkHealth = 1.0;
-    public long twerkDelayTime = 0;
+    public double twerkHealth = getConfig().getDouble("defaultTwerkHealth");
+    public long twerkDelayTime = getConfig().getLong("defaultTwerkDelay");
+    public int disableTwerkConfigCommands = getConfig().getInt("disableTwerkConfigCommands");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -40,48 +42,106 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
         if(command.getName().equalsIgnoreCase("twerkHealth")){
 
 
-            if (sender instanceof Player) {
-                if (args.length > 0) {
-                    try {
-                        twerkHealth = Double.parseDouble(args[0]);
-                        p.sendMessage(ChatColor.DARK_GREEN + "Shifting now gives " + ChatColor.RED + twerkHealth + " HP");
-                    } catch (IllegalArgumentException e) {
+            if (disableTwerkConfigCommands == 1){
+                if (sender instanceof Player) {
+                    if (args.length > 0) {
+                        try {
+                            twerkHealth = Double.parseDouble(args[0]);
+                            p.sendMessage(ChatColor.DARK_GREEN + "Shifting now gives " + ChatColor.RED + twerkHealth + " HP");
+                        } catch (IllegalArgumentException e) {
+                            p.sendMessage(ChatColor.RED + "You must give a double as an argument");
+                            p.sendMessage(ChatColor.RED + "Ex: 0.1, 6.9, 4.20 & etc.");
+                        }
+                    } else {
                         p.sendMessage(ChatColor.RED + "You must give a double as an argument");
                         p.sendMessage(ChatColor.RED + "Ex: 0.1, 6.9, 4.20 & etc.");
                     }
-                } else {
-                    p.sendMessage(ChatColor.RED + "You must give a double as an argument");
-                    p.sendMessage(ChatColor.RED + "Ex: 0.1, 6.9, 4.20 & etc.");
+                }else{
+                    System.out.println("You must be a player to use this command");
                 }
+            }else if (disableTwerkConfigCommands == 2){
+                p.sendMessage(ChatColor.RED + "This command has been disabled by the owner of the server");
+                p.sendMessage(ChatColor.RED + "If this is a mistake, contact the owner of the server");
             }else{
-                System.out.println("You must be a player to use this command");
+                if (sender instanceof Player) {
+                    if (args.length > 0) {
+                        try {
+                            twerkHealth = Double.parseDouble(args[0]);
+                            p.sendMessage(ChatColor.DARK_GREEN + "Shifting now gives " + ChatColor.RED + twerkHealth + " HP");
+                        } catch (IllegalArgumentException e) {
+                            p.sendMessage(ChatColor.RED + "You must give a double as an argument");
+                            p.sendMessage(ChatColor.RED + "Ex: 0.1, 6.9, 4.20 & etc.");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED + "You must give a double as an argument");
+                        p.sendMessage(ChatColor.RED + "Ex: 0.1, 6.9, 4.20 & etc.");
+                    }
+                }else{
+                    System.out.println("You must be a player to use this command");
+                }
             }
+
         }else if(command.getName().equalsIgnoreCase("noTwerk")){
 
-            if (sender instanceof Player){
+            if (disableTwerkConfigCommands == 1){
+                if (sender instanceof Player){
 
-                p.sendMessage(ChatColor.DARK_GREEN + "Shift Healing is now Disabled");
-                twerkHealth = 0;
+                    p.sendMessage(ChatColor.DARK_GREEN + "Shift Healing is now Disabled");
+                    twerkHealth = 0;
+                }else{
+                    System.out.println("You must be a player to use this command");
+                }
+            }else if (disableTwerkConfigCommands == 2){
+                p.sendMessage(ChatColor.RED + "This command has been disabled by the owner of the server");
+                p.sendMessage(ChatColor.RED + "If this is a mistake, contact the owner of the server");
             }else{
-                System.out.println("You must be a player to use this command");
+                if (sender instanceof Player){
+
+                    p.sendMessage(ChatColor.DARK_GREEN + "Shift Healing is now Disabled");
+                    twerkHealth = 0;
+                }else{
+                    System.out.println("You must be a player to use this command");
+                }
             }
 
         }else if(command.getName().equalsIgnoreCase("twerkDelay")){
-            if (sender instanceof Player) {
-                if (args.length > 0) {
-                    try {
-                        twerkDelayTime = Long.parseLong(args[0]);
-                        p.sendMessage(ChatColor.DARK_GREEN + "There is now a " + ChatColor.RED + twerkDelayTime + ChatColor.DARK_GREEN + " millisecond delay");
-                    } catch (IllegalArgumentException e) {
+            if (disableTwerkConfigCommands == 1){
+                if (sender instanceof Player) {
+                    if (args.length > 0) {
+                        try {
+                            twerkDelayTime = Long.parseLong(args[0]);
+                            p.sendMessage(ChatColor.DARK_GREEN + "There is now a " + ChatColor.RED + twerkDelayTime + ChatColor.DARK_GREEN + " millisecond delay");
+                        } catch (IllegalArgumentException e) {
+                            p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
+                            p.sendMessage(ChatColor.RED + "Ex: 0, 69, 420 & etc.");
+                        }
+                    } else {
                         p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
-                        p.sendMessage(ChatColor.RED + "Ex: 0, 69, 420 & etc.");
+                        p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
                     }
-                } else {
-                    p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
-                    p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
+                }else{
+                    System.out.println("You must be a player to use this command");
                 }
+            }else if (disableTwerkConfigCommands == 2){
+                p.sendMessage(ChatColor.RED + "This command has been disabled by the owner of the server");
+                p.sendMessage(ChatColor.RED + "If this is a mistake, contact the owner of the server");
             }else{
-                System.out.println("You must be a player to use this command");
+                if (sender instanceof Player) {
+                    if (args.length > 0) {
+                        try {
+                            twerkDelayTime = Long.parseLong(args[0]);
+                            p.sendMessage(ChatColor.DARK_GREEN + "There is now a " + ChatColor.RED + twerkDelayTime + ChatColor.DARK_GREEN + " millisecond delay");
+                        } catch (IllegalArgumentException e) {
+                            p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
+                            p.sendMessage(ChatColor.RED + "Ex: 0, 69, 420 & etc.");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED + "You must give an integer/long as an argument");
+                        p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
+                    }
+                }else{
+                    System.out.println("You must be a player to use this command");
+                }
             }
         }
 
@@ -122,9 +182,9 @@ public final class HealthyTwerking extends JavaPlugin implements Listener {
                 Thread.currentThread().interrupt();
             }
         }else if (twerkDelayTime < 0){
-            p.sendMessage(ChatColor.RED + "You must give a positive number as an argument");
+            p.sendMessage(ChatColor.RED + "You must give a positive number as an argument for the /twerkDelay command");
         }else{
-            p.sendMessage(ChatColor.RED + "Please give a valid argument");
+            p.sendMessage(ChatColor.RED + "Please give a valid argument for the /twerkDelay command");
             p.sendMessage(ChatColor.RED + "Ex: 01, 69, 420 & etc.");
         }
     }
